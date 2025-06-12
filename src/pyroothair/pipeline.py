4a315c5c1ac167abb612c26e09a7fa1eed0e99bd
iantsang779 <ian.tsang@niab.com>
2025-06-12 10:36:12 +0100

    fixes to handle pixel intensity

--- src/pyroothair/pipeline.py
+++ src/pyroothair/pipeline.py
@@ -19,7 +19,7 @@ class CheckArgs():
 
     def check_arguments_gpu(self) -> None:
         """
-        Check necessary arguments if GPU is available
+        Check main arguments
         """
         missing_args = []
             # check necessary arguments are supplied
@@ -30,62 +30,63 @@ class CheckArgs():
         if self.args.save_path is None:
             missing_args.append('-o/--output')
         if missing_args:
-            self.parser.error(f'The following arguments are required when running pyRootHair using the main pipeline: {missing_args}')
+            self.parser.error(f'The following arguments are required for pyroothair: {missing_args}')
+
+    def check_arguments_train_rfc(self) -> None:
+        """
+        Check train_random_forest arguments
+        """
+        missing_args = []
+
+        if self.args.train_img_path is None:
+            missing_args.append('--train_img')
+        if self.args.train_mask_path is None:
+            missing_args.append('--train_mask')
+        if self.args.model_output_path is None:
+            missing_args.append('--model_output')
+        if missing_args:
+            self.parser.error(f'The following arguments are required for pyroothair_train_random_forest: {missing_args}')
 
     def check_arguments_rfc(self)-> None:
         """
-        Check necessary arguments if GPU is not available
+        Check run_random_forest arguments
         """
+        missing_args = []
 
         if self.args.rfc_model_path is None:
-            self.parser.error('The following argument is required if using a Random Forest Classifier: --rfc_model_path.')
+            missing_args.append('-rfc/--rfc_model_path')
         if self.args.img_dir is None:
-            self.parser.error('Missing argument for the input image directory -i/--input.')
-        if self.args.input_mask:
-            self.parser.error('Conflicting arguments: --input_mask is not compatible with --rfc_model_path.')
+            self.parser.error('-i/--input')
         if self.args.batch_id is None:
-            self.parser.error('Missing argument for batch_id: -b/--batch_id')
+            self.parser.error('-b/--batch_id')
         if self.args.save_path is None:
-            self.parser.error('Missing argument for output: -o/--output')
-
-    def check_arguments_output(self) -> None:
-        """
-        Check --output argument is provided
-        """
-        if self.args.save_path is None:
-            self.parser.error('Please specify filepath to store output data with -o/--output.')
-
+            self.parser.error('-o/--output')
+        if missing_args:
+            self.parser.error(f'The following arguments are required for pyroothair_run_random_forest: {missing_args}')
 
     def check_arguments_single_mask(self) -> None:
         """
-        Check necessary arguments if processing a single binary mask
+        Check run_single_mask arguments
         """
         missing_args = []
-        if self.args.rfc_model_path:
-            self.parser.error('Conflicting arguments: --input_mask was specified, which is not compatible with --rfc_model_path.')
-        if self.args.img_dir:
-            self.parser.error('Conflicting arguments: -i/--input is for a directory containing a batch of images intended for batch processing. Please use --input_mask instead for a single mask.')
+        
+        if self.args.input_mask is None:
+            missing_args.append('-m/--mask')
+        if self.args.input_image is None:
+            missing_args.append('-i/--image')
         if self.args.save_path is None:
             missing_args.append('-o/--output')
         if self.args.batch_id is None:
             missing_args.append('-b/--batch_id')
-        if self.args.input_mask is None:
-            missing_args.append('--input_mask')
         if missing_args:
-            self.parser.error(f'The following arguments are required when using `-p single` for processing an individual mask: {missing_args}')
+            self.parser.error(f'The following arguments are required for pyroothair_run_single_mask: {missing_args}')
     
     def check_argument_demo(self) -> None:
         """
         Check necessary arguments for pyroothair_run_demo
-        """
-        missing_args = []
-        
-        if self.args.batch_id is None:
-            missing_args.append('-b/--batch_id')
+        """        
         if self.args.save_path is None:
-            missing_args.append('-o/--output')
-        if missing_args:
-            self.parser.error(f'The following arguments are required when running pyroothair_run_demo: {missing_args}')
+            self.parser.error(f'The following argument is required when running pyroothair_run_demo: -o/--output')
         
 
     def convert_mask(self, mask: 'NDArray') -> 'NDArray':
@@ -107,8 +108,10 @@ class Pipeline(CheckArgs):
     """
     Run core pipeline to mine traits from binary masks generated from inference
     """
-    def __init__(self, check_args) -> None:
+    def __init__(self, check_args, input_img:'NDArray', input_img_path: str) -> None:
+        self.input_img = input_img
         self.check_args = check_args
+        self.input_img_path = input_img_path
         self.args = check_args.args
         self.parser = check_args.parser
         if self.args.save_path:
@@ -121,7 +124,9 @@ class Pipeline(CheckArgs):
         """
         Core pipeline bringing together logic across all modules
         """
-        root_mask = (init_mask == 2)
+        root_mask = init_mask == 2
+        rh_mask = init_mask == 1
+        bg_mask = init_mask == 0
 
         skeleton = Skeleton() 
         clean_root = skeleton.extract_root(root_mask)
@@ -149,7 +154,8 @@ class Pipeline(CheckArgs):
         root_hairs_cropped = rt.crop_rh_mask(root_hairs)
 
         if root_hairs_cropped.shape != (1,1):
-            data = GetParams(root_hairs_cropped)
+            data = GetParams(root_hairs_cropped, self.input_img, self.input_img_path, rh_mask, bg_mask)
+            data.calculate_pixel_intensity()
             data.sliding_window(self.args.height_bin_size)
             data.clean_data()
             data.calibrate_data(self.args.conv)
@@ -159,15 +165,12 @@ class Pipeline(CheckArgs):
             summary_df, raw_df = data.generate_table(filename.split('.')[0], self.args.batch_id, root_thickness, self.args.conv)
 
             if self.args.show_summary:
-                self.check_args.check_arguments_output()
                 data.plot_summary(self.plots_path, filename.split('.')[0])
 
             if self.args.show_transformation:
-                self.check_args.check_arguments_output()
                 skeleton.visualize_transformation(rotated_mask, self.plots_path, filename.split('.')[0]) 
 
             if self.args.show_segmentation:
-                self.check_args.check_arguments_output()
                 plt.imsave(os.path.join(self.plots_path,f'{filename.split('.')[0]}_mask.png'), straight_mask)
                 plt.imsave(os.path.join(self.plots_path,f'{filename.split('.')[0]}_root_hair_mask_cropped.png'), root_hairs_cropped)
 
@@ -175,8 +178,7 @@ class Pipeline(CheckArgs):
 
         else:
             if self.args.show_segmentation:
-                self.check_args.check_arguments_output()
                 plt.imsave(os.path.join(self.plots_path,f'{filename.split('.')[0]}_mask.png'), straight_mask)
                 plt.imsave(os.path.join(self.plots_path,f'{filename.split('.')[0]}_root_hair_mask.png'), root_hairs)
 
-            return [], []
+            return pd.DataFrame(), pd.DataFrame()
